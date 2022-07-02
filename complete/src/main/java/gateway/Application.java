96762b9becd69e4bed73ca7eee88482b16aa3927
abourya <mabourya@lnddo.com>
2022-07-02 14:20:48 +0200

    spring cloud gateway

--- complete/src/main/java/gateway/Application.java
+++ complete/src/main/java/gateway/Application.java
@@ -1,5 +1,6 @@
 package gateway;
 
+import org.springframework.util.Base64Utils;
 import reactor.core.publisher.Mono;
 
 import org.springframework.boot.SpringApplication;
@@ -18,51 +19,64 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-	// tag::route-locator[]
-	@Bean
-	public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
-		String httpUri = uriConfiguration.getHttpbin();
-		return builder.routes()
-			.route(p -> p
-				.path("/get")
-				.filters(f -> f.addRequestHeader("Hello", "World"))
-				.uri(httpUri))
-			.route(p -> p
-				.host("*.circuitbreaker.com")
-				.filters(f -> f
-					.circuitBreaker(config -> config
-						.setName("mycmd")
-						.setFallbackUri("forward:/fallback")))
-				.uri(httpUri))
-			.build();
-	}
-	// end::route-locator[]
+    // tag::route-locator[]
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
+//        String httpUri = uriConfiguration.getHttpbin();
+//        String finerAcrUrl = uriConfiguration.getHttpfin();
+        RouteLocator routerLocator = builder.routes()
+                .route(p -> p
+                        .path("/**")
+                        .filters(f ->
+                                f.addRequestHeader("Fineract-Platform-TenantId", "default")
+                                        .addRequestHeader("Authorization", String.format("%s", getCredentials("mifos", "password")))
+                                        .addRequestHeader("Content-Type", "application/json; charset=utf8"))
+                        .uri("https://api.lnddo.loan"))
+//				.route(p -> p
+//						.path("/**")
+//						.filters(f -> f.addRequestHeader("X-RapidAPI-Key", "8114e06e6amsh1703b16c92d7c73p19b91bjsnfbd5b17004ab")
+//								.addRequestHeader("X-RapidAPI-Host", "weatherbit-v1-mashape.p.rapidapi.com"))
+//						.uri("https://weatherbit-v1-mashape.p.rapidapi.com/"))
+                .build();
+        return routerLocator;
+    }
 
-	// tag::fallback[]
-	@RequestMapping("/fallback")
-	public Mono<String> fallback() {
-		return Mono.just("fallback");
-	}
-	// end::fallback[]
+    private String getCredentials(String username, String password) {
+        byte[] encodedBytes = Base64Utils.encode((username + ":" + password).getBytes());
+        return "Basic " + new String(encodedBytes);
+    }
+    // end::route-locator[]
+
+    // tag::fallback[]
+    @RequestMapping("/fallback")
+    public Mono<String> fallback() {
+        return Mono.just("fallback");
+    }
+    // end::fallback[]
 }
 
 // tag::uri-configuration[]
 @ConfigurationProperties
 class UriConfiguration {
-	
-	private String httpbin = "http://httpbin.org:80";
 
-	public String getHttpbin() {
-		return httpbin;
-	}
+    private String httpbin = "https://dashboard-qa.htagdev.info/";
+    private String finerAcrUrl = "https://api.lnddo.loan/";
 
-	public void setHttpbin(String httpbin) {
-		this.httpbin = httpbin;
-	}
+    public String getHttpbin() {
+        return httpbin;
+    }
+
+    public String getHttpfin() {
+        return finerAcrUrl;
+    }
+
+    public void setHttpbin(String httpbin) {
+        this.httpbin = httpbin;
+    }
 }
 // end::uri-configuration[]
 // end::code[]
